6b12f48f120dbbd781162ca9ee08cc351562a9ef
olu26 <oluwork12@gmail.com>
2025-07-17 16:04:41 +0100

    resolveing conflict 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import { Navbar } from "./pages/Navbar";
 import { Home } from "./pages/Home";
+import React from 'react';
+
 import { PhoneCard } from "./pages/PhoneCard";
 
 function App() {
